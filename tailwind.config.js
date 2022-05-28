c4bc571db77af87980223bc5a0148c12fbd6990b
William <william@pixney.com>
2022-05-28 20:44:00 +0200

    hamburger

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   mode: 'jit',
+  safelist: ['c-hamburger', 'c-hamburger__line', 'c-hamburger__line:after'],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
     require('./src/plugins/tailwind.scroller'),
-  ],
+    require('./src/plugins/tailwind.hamburger')
+  ]
 }
