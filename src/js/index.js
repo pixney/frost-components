ccd9013efee47560d2c6ae5a787c2eb9397e7f17
William Åström <william@pixney.com>
2022-05-29 01:53:37 +0200

    Navigation and hamurger component
    * Hamburger component
    * Mobile and Desktop navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import Alpine from 'alpinejs'
 import Scroller from './components/scroller'
-import '../css/style.css';
+import Navigation from './components/navigation'
+import '../css/style.css'
 window.Alpine = Alpine
-Alpine.data('scroller',Scroller)
+Alpine.data('scroller', Scroller)
+Alpine.data('navigation', Navigation)
 Alpine.start()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   mode: 'jit',
+  safelist: [],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
     require('./src/plugins/tailwind.scroller'),
-  ],
+    require('./src/plugins/tailwind.hamburger'),
+    require('./src/plugins/tailwind.navigation'),
+    require('./src/plugins/tailwind.mobileNav')
+  ]
 }
