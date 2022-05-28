285879757a87b2aa7ec68ae7443b3e0661ea46de
William <william@pixney.com>
2022-05-28 19:28:40 +0200

    Responsive mobile navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import Alpine from 'alpinejs'
 import Scroller from './components/scroller'
-import '../css/style.css';
+import NavigationMobile from './components/navigationMobile'
+import '../css/style.css'
 window.Alpine = Alpine
-Alpine.data('scroller',Scroller)
+Alpine.data('scroller', Scroller)
+Alpine.data('navigationMobile', NavigationMobile)
 Alpine.start()
